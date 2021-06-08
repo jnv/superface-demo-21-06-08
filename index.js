e4c5ae8dcc99b20abf35b8ab8b6cf3591871403d
Jan Vlnas <>
2021-06-08 09:16:38 +0000

    Implement getRepos

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const {inspect} = require('util')
 const { SuperfaceClient } = require('@superfaceai/one-sdk')
+const sdk = new SuperfaceClient()
 
 async function getRepos(username) {
-  return []
+  const profile = await sdk.getProfile('vcs/user-repos')
+  const result = await profile.getUseCase('UserRepos').perform({user: username})
+  return result.value.repos
 }
 
 async function main() {
-  const repos = await getRepos('github')
+  const repos = await getRepos('superfaceai')
   console.log(repos)
 }
+
+main()
