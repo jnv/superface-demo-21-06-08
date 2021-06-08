3be639fe6b2e0f3a672786742b4739c684a0756e
Jan Vlnas <jnv@users.noreply.github.com>
2021-06-08 12:12:20 +0000

    Initial blank state

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 require('dotenv').config()
-const { SuperfaceClient } = require('@superfaceai/one-sdk')
-const sdk = new SuperfaceClient()
 
-const MAIL_TO = 'jan.vlnas@superface.ai'
-
-async function getRepos(repoProvider, username) {
-  const profile = await sdk.getProfile('vcs/user-repos')
-  const provider = await sdk.getProvider(repoProvider);
-  const result = await profile.getUseCase('UserRepos').perform({user: username}, {provider})
-  return result.value.repos
+async function getRepos(username) {
+  return []
 }
 
-async function sendEmail(repos = []) {
-  const profile = await sdk.getProfile('communication/send-email')
-  const text = JSON.stringify(repos, undefined, 2)
-  const result = await profile
-    .getUseCase('SendEmail')
-    .perform({to: MAIL_TO, subject: 'Your requested repositories', text})
-  console.log(result)
+async function main(username) {
+  const repos = await getRepos(username)
+  console.log(repos)
 }
 
-async function main(repoProvider, username) {
-  const repos = await getRepos(repoProvider, username)
-  await sendEmail(repos)
-  // console.log(repos)
-}
-
-main(process.argv[2], process.argv[3])
+main(process.argv[2])
