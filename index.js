bf893cf882362a6c233d08195eae76eeccd3972f
Jan Vlnas <>
2021-06-08 08:50:55 +0000

    Update one-sdk

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { SuperfaceClient } = require('@superfaceai/one-sdk')
 
 async function getRepos(username) {
-  
+  return []
 }
 
 async function main() {
   const repos = await getRepos('github')
+  console.log(repos)
 }
