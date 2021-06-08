5464d9ca4cec3af5e21a9e9b0c960a6166b1795a
Jan Vlnas <>
2021-06-08 09:35:19 +0000

    Implement provider switching

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const {inspect} = require('util')
 const { SuperfaceClient } = require('@superfaceai/one-sdk')
 const sdk = new SuperfaceClient()
 
-async function getRepos(username) {
+async function getRepos(repoProvider, username) {
   const profile = await sdk.getProfile('vcs/user-repos')
-  const result = await profile.getUseCase('UserRepos').perform({user: username})
+  const provider = await sdk.getProvider(repoProvider);
+  const result = await profile.getUseCase('UserRepos').perform({user: username}, {provider})
   return result.value.repos
 }
 
-async function main(username) {
-  const repos = await getRepos(username)
+async function main(repoProvider, username) {
+  const repos = await getRepos(repoProvider, username)
   console.log(repos)
 }
 
-main(process.argv[2])
+main(process.argv[2], process.argv[3])
